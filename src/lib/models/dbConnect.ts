108cadcba657dea3475615ef336940a7d1b02ccb
Chumar11 <chumarsarwar3@gmail.com>
2025-02-27 20:37:21 +0500

    g

--- src/lib/models/dbConnect.ts
+++ src/lib/models/dbConnect.ts
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 const database =
   "mongodb+srv://2223016:N6bVPiFwmE5hhoI5@cluster0.3dvrp.mongodb.net/test?retryWrites=true&w=majority";
 // const database = "mongodb://localhost:27017/limo";
+console.log("data",database)
 let isConnected = false;
 
 export const dbConnect = async () => {
